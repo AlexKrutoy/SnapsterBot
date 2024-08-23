070eb26f11867acab4b6442fcaa5c7ffab65c1ad
DeniedAccessLife <89941208+DeniedAccessLife@users.noreply.github.com>
2024-08-23 04:21:08 +0300

    Add files via upload

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -1,22 +1,21 @@
 import asyncio
 from urllib.parse import unquote, quote
-
 import aiohttp
 import json
+import html
 from aiocfscrape import CloudflareScraper
 from aiohttp_proxy import ProxyConnector
 from better_proxy import Proxy
 from pyrogram import Client
 from pyrogram.errors import Unauthorized, UserDeactivated, AuthKeyUnregistered, FloodWait
 from pyrogram.raw.functions.messages import RequestWebView
-from datetime import datetime, timedelta
+from datetime import datetime
+import random
 from .agents import generate_random_user_agent
-
 from bot.utils import logger
 from bot.exceptions import InvalidSession
 from .headers import headers
 from bot.config import settings
-
 
 class Tapper:
     def __init__(self, tg_client: Client):
@@ -25,121 +24,66 @@ class Tapper:
         self.user_id = 0
 
     async def get_tg_web_data(self, proxy: str | None) -> str:
-        if proxy:
-            proxy = Proxy.from_str(proxy)
-            proxy_dict = dict(
-                scheme=proxy.protocol,
-                hostname=proxy.host,
-                port=proxy.port,
-                username=proxy.login,
-                password=proxy.password
-            )
-        else:
-            proxy_dict = None
-
+        proxy_dict = Proxy.from_str(proxy).as_dict() if proxy else None
         self.tg_client.proxy = proxy_dict
-
+    
         try:
-            with_tg = True
-
             if not self.tg_client.is_connected:
-                with_tg = False
-                try:
-                    await self.tg_client.connect()
-                    start_command_found = False
-
-                    async for message in self.tg_client.get_chat_history('snapster_bot'):
-                        if (message.text and message.text.startswith('/start')) or (message.caption and message.caption.startswith('/start')):
-                            start_command_found = True
-                            break
-
-                    if not start_command_found:
-                        if settings.REF_ID == '':
-                            await self.tg_client.send_message("snapster_bot", "/start 737844465")
-                        else:
-                            await self.tg_client.send_message("snapster_bot", f"/start {settings.REF_ID}")
-                except (Unauthorized, UserDeactivated, AuthKeyUnregistered):
-                    raise InvalidSession(self.session_name)
-
+                await self.tg_client.connect()
+                async for message in self.tg_client.get_chat_history('snapster_bot'):
+                    if message.text and message.text.startswith('/start'):
+                        break
+                else:
+                    ref_id = settings.REF_ID or 'ref_acs_c-Zl_DyApG'
+                    await self.tg_client.send_message("snapster_bot", f"/start {ref_id}")
+    
             while True:
                 try:
                     peer = await self.tg_client.resolve_peer('snapster_bot')
                     break
                 except FloodWait as fl:
-                    fls = fl.value
-
                     logger.warning(f"{self.session_name} | FloodWait {fl}")
-                    logger.info(f"{self.session_name} | Sleep {fls}s")
-
-                    await asyncio.sleep(fls + 3)
-
-            web_view = await self.tg_client.invoke(RequestWebView(
-                peer=peer,
-                bot=peer,
-                platform='android',
-                from_bot_menu=False,
-                url='https://snapster-lake.vercel.app/'
-            ))
+                    logger.info(f"{self.session_name} | Sleep {fl.value}s")
+                    await asyncio.sleep(fl.value + 3)
+    
+            web_view = await self.tg_client.invoke(RequestWebView(peer=peer, bot=peer, platform='android',from_bot_menu=False, url='https://snapster.psylabs.tech/'))
 
             auth_url = web_view.url
-            tg_web_data = unquote(
-                string=unquote(
-                    string=auth_url.split('tgWebAppData=', maxsplit=1)[1].split('&tgWebAppVersion', maxsplit=1)[0]))
+            tg_web_data = unquote(string=unquote(string=auth_url.split('tgWebAppData=', maxsplit=1)[1].split('&tgWebAppVersion', maxsplit=1)[0]))
 
             self.user_id = (await self.tg_client.get_me()).id
 
-            if with_tg is False:
+            if not self.tg_client.is_connected:
                 await self.tg_client.disconnect()
 
             return tg_web_data
-
-        except InvalidSession as error:
-            raise error
-
+    
+        except (Unauthorized, UserDeactivated, AuthKeyUnregistered):
+            raise InvalidSession(self.session_name)
         except Exception as error:
-            escaped_error = str(error).replace('<', '&lt;').replace('>', '&gt;')
-            logger.error(f"{self.session_name} | Unknown error during Authorization: {escaped_error}")
-            await asyncio.sleep(delay=3)
-
-    async def get_stats(self, http_client: aiohttp.ClientSession):
-        response_text = ""
-        try:
-            async with http_client.get(url='https://45.87.154.135/api/user') as response:
-                response_text = await response.text()
-                if response_text:
-                    try:
-                        data = json.loads(response_text)
-                        points = data.get('points')
-                        last_claim = data.get('dateLastClaimed')
-                        return (points, last_claim)
-                    except json.JSONDecodeError as error:
-                        escaped_error = str(error).replace('<', '&lt;').replace('>', '&gt;')
-                        logger.error(f"{self.session_name} | JSON decode error: {escaped_error}")
-                        logger.error(f"{self.session_name} | Response: {response}")
-                        logger.error(f"{self.session_name} | headers: {http_client.headers}")
-                        logger.error(f"{self.session_name} | Headers response: {response.headers}")
-                        logger.error(f"{self.session_name} | Response text: {response_text.encode('unicode_escape')}")
-                        return None, None
-                else:
-                    logger.error(f"{self.session_name} | Empty response received")
-                    return None, None
-        except Exception as error:
-            escaped_error = str(error).replace('<', '&lt;').replace('>', '&gt;')
-            logger.error(f"{self.session_name} | Error happened: {escaped_error}")
-            logger.error(f"{self.session_name} | Response: {response_text.encode('unicode_escape')}")
-            logger.error(f"{self.session_name} | Headers: {response.headers}")
-            logger.error(f"{self.session_name} | headers: {http_client.headers}")
-            logger.error(f"{self.session_name} | Response: {response}")
-            return None, None
+            logger.error(f"{self.session_name} | Unknown error during Authorization: {html.escape(str(error))}")
+            await asyncio.sleep(3)
 
     async def daily_claim(self, http_client: aiohttp.ClientSession) -> bool:
+        url = "https://snapster.psylabs.tech/api/user/claimMiningBonus"
+        payload = { "telegramId": f"{self.user_id}" }
+
         try:
-            async with http_client.post(url="https://45.87.154.135/api/claim-daily"):
-                return True
+            async with http_client.post(url=url, json=payload) as response:
+                if response.status == 200:
+                    response_data = await response.json()
+                    if response_data.get("result"):
+                        points_claimed = response_data["data"]["pointsClaimed"]
+                        points_count = response_data["data"]["user"]["pointsCount"]
+                        logger.success(f"{self.session_name} | Points claimed: {points_claimed}, Total points: {points_count}")
+                        return True
+                    else:
+                        logger.error(f"{self.session_name} | Daily claim failed: {json.dumps(response_data)}")
+                else:
+                    logger.error(f"{self.session_name} | Unexpected response: {response.status}, {html.escape(await response.text())}")
         except Exception as error:
-            escaped_error = str(error).replace('<', '&lt;').replace('>', '&gt;')
-            logger.error(f"{self.session_name} | Daily claim error: {escaped_error}")
-            return False
+            logger.error(f"{self.session_name} | Daily claim error: {html.escape(str(error))}")
+        return False
 
     async def check_proxy(self, http_client: aiohttp.ClientSession, proxy: Proxy) -> None:
         try:
@@ -159,54 +103,39 @@ class Tapper:
             tg_web_data = await self.get_tg_web_data(proxy=proxy)
             while True:
                 try:
-                    tg_web_data_parts = tg_web_data.split('&')
-                    query_id = tg_web_data_parts[0].split('=')[1]
-                    user_data = tg_web_data_parts[1].split('=')[1]
-                    auth_date = tg_web_data_parts[2].split('=')[1]
-                    hash_value = tg_web_data_parts[3].split('=')[1]
-                    
-                    user_data_encoded = quote(user_data)
-                    init_data = f"query_id={query_id}&user={user_data_encoded}&auth_date={auth_date}&hash={hash_value}"
-                    http_client.headers['x-telegram-auth'] = f"{init_data}"
-                    http_client.headers['User-Agent'] = generate_random_user_agent(device_type='android',
-                                                                           browser_type='chrome')
-
                     if not tg_web_data:
                         continue
-                    
-                    points, last_claim = await self.get_stats(http_client=http_client)
-                    if points is None and last_claim is None:
-                        logger.info(f"{self.session_name} | Bot is lagging, retrying...")
-                        await asyncio.sleep(3)
-                        continue
 
-                    logger.info(f"{self.session_name} | Your points right now: {points}")
+                    tg_web_data_parts = tg_web_data.split('&')
+                    query_id = tg_web_data_parts[0].split('=')[1]
+                    user_data = quote(tg_web_data_parts[1].split('=')[1])
+                    auth_date = tg_web_data_parts[2].split('=')[1]
+                    hash_value = tg_web_data_parts[3].split('=')[1]
 
-                    await asyncio.sleep(2)
+                    init_data = f"query_id={query_id}&user={user_data}&auth_date={auth_date}&hash={hash_value}"
+                    http_client.headers['Telegram-Data'] = init_data
+                    http_client.headers['User-Agent'] = generate_random_user_agent(device_type='android', browser_type='chrome')
 
-                    current_time = datetime.now()
-                    convert = datetime.strptime(last_claim, "%Y-%m-%dT%H:%M:%S.%fZ")
-                    convert += timedelta(hours=27)
-                    if current_time >= convert:
-                        status = await self.daily_claim(http_client=http_client)
-                        if status is True:
-                            logger.success(f"{self.session_name} | Daily claim successful")
+                    status = await self.daily_claim(http_client=http_client)
+                    if status:
+                        delay = random.randint(3600, 21600)
+                        hours = delay // 3600
+                        minutes = (delay % 3600) // 60
+                        seconds = delay % 60
+                        logger.debug(f"{self.session_name} | Sleep {hours}h {minutes}m {seconds}s")
+                        await asyncio.sleep(delay)
                     else:
-                        logger.info(f"{self.session_name} | Can`t daily claim, going sleep 1 hour")
-                        await asyncio.sleep(delay=3600)
-                    await asyncio.sleep(5)
+                        await asyncio.sleep(6)
 
                 except InvalidSession as error:
                     raise error
 
                 except Exception as error:
-                    escaped_error = str(error).replace('<', '&lt;').replace('>', '&gt;')
-                    logger.error(f"{self.session_name} | Unknown error: {escaped_error}")
-                    await asyncio.sleep(delay=3)
-
+                    logger.error(f"{self.session_name} | Unknown error: {html.escape(str(error))}")
+                    await asyncio.sleep(3)
 
 async def run_tapper(tg_client: Client, proxy: str | None):
     try:
         await Tapper(tg_client=tg_client).run(proxy=proxy)
     except InvalidSession:
-        logger.error(f"{tg_client.name} | Invalid Session")
+        logger.error(f"{tg_client.name} | Invalid Session!")
--- bot/utils/logger.py
+++ bot/utils/logger.py
@@ -1,10 +1,6 @@
 import sys
 from loguru import logger
 
-
 logger.remove()
-logger.add(sink=sys.stdout, format="<white>{time:YYYY-MM-DD HH:mm:ss}</white>"
-                                   " | <level>{level: <8}</level>"
-                                   " | <cyan><b>{line}</b></cyan>"
-                                   " - <white><b>{message}</b></white>")
+logger.add(sink=sys.stdout, format="<white><b>{time:YYYY-MM-DD HH:mm:ss}</b></white> | <level>{level: <8}</level> | <cyan><b>{line}</b></cyan> - <white><b>{message}</b></white>")
 logger = logger.opt(colors=True)
